55c4c3f53cac5e9e6c1a989377659f304359aa15
EduardoAlmeida10 <eduardoalmeida101325@gmail.com>
2022-12-03 20:57:20 -0300

    Toast

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,5 @@
 /* importação da imagem, pegando a local de outra pasta. */
+import { toast } from 'react-toastify';
 import axios from 'axios';
 import icon from '../../assets/img/notification-icon.svg';
 import { BASE_URL } from '../../utils/request';
@@ -12,7 +13,7 @@ type Props = {
 function handleClick(id : number) {
     axios(`${BASE_URL}/sales/${id}/notification`)
         .then(response => {
-            console.log("SUCESSO");
+            toast.info("SMS enviado com sucesso");
         });
 }
 
